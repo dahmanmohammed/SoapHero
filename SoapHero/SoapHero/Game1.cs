e6f290f93c9dd63b1e6c54de020bb60508515399
dahman <26053267+dahmanmohammed@users.noreply.github.com>
2017-10-30 21:56:33 -0400

    bleuViolet

--- SoapHero/SoapHero/Game1.cs
+++ SoapHero/SoapHero/Game1.cs
@@ -73,7 +73,7 @@ namespace SoapHero
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Blue);
+            GraphicsDevice.Clear(Color.BlueViolet);
 
             // TODO: Add your drawing code here
 
